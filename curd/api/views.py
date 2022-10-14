31ef2ff9065eb5294fb4426511d204d75327ceca
Prashant2000Tiwari <91279902+Prashant2000Tiwari@users.noreply.github.com>
2022-10-14 13:43:23 +0530

    Add files via upload

---
+++ curd/api/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from api.models import Company,Employee
+from api.serializers import CompanySerializer,EmployeeSerializer
+from rest_framework.decorators import action
+
+# Create your views here.
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset= Company.objects.all()
+    serializer_class=CompanySerializer
+    
+    
+    
+class EmployeeViewSet(viewsets.ModelViewSet):
+	queryset= Employee.objects.all()
+	serializer_class=EmployeeSerializer
+    
+
